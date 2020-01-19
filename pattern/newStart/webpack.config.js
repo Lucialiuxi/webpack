1cd47aa8c45d808588d2648491e1881caf107150
lucia <lucia@seektopser.com>
2020-01-19 11:53:28 +0800

    增加gitignore文件

--- pattern/newStart/webpack.config.js
+++ pattern/newStart/webpack.config.js
@@ -1,12 +1,13 @@
 const path = require("path");
  //把css提取到单独的文件中的插件
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+// const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const chalk = require('chalk');
 
 //创建多个实例
-const extractLESS = new ExtractTextPlugin('stylesheets/[name]-two.css');
-const extractCSS = new ExtractTextPlugin('stylesheets/[name]-one.css');
+// const extractLESS = new ExtractTextPlugin('stylesheets/[name]-two.css');
+// const extractCSS = new ExtractTextPlugin('stylesheets/[name]-one.css');
 
 // 编辑进度条
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
@@ -18,48 +19,52 @@ const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 
 module.exports = {
     mode: "development",
-    entry: "./main.jsx",
+    entry: { index: "./src/index.js" },
     output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname , "./dist"),
+        filename: "[name].js",
+        path: path.join(__dirname , "./dist"),
     },
-    resolve: {
-        extensions: ['.jsx', '.js', '.less', '.css', '.json',]
-    },
+    // resolve: {
+    //     extensions: ['.jsx', '.js', '.less', '.css', '.json',]
+    // },
     module: {
         rules: [
             {
-                test: '/\.js[x]?$/',
-                exclude: '/(node_modules|bower_components)/',
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-react"],
-                        plugins: ['@babel/transform-runtime'],
-                    }
-                },
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: "babel-loader",
             },
-            {
-                test:/\.css$/,
-                //把css提取到单独的文件中
-                use: extractCSS.extract({
-                    fallback: "style-loader",
-                    use: "css-loader"
-                })
-            },
-            {
-              test: /\.less$/,
-              use: extractLESS.extract([ 'css-loader', 'less-loader' ])
-            }
+            // {
+            //     test:/\.css$/,
+            //     //把css提取到单独的文件中
+            //     use: extractCSS.extract({
+            //         fallback: "style-loader",
+            //         use: "css-loader"
+            //     })
+            // },
+            // {
+            //   test: /\.less$/,
+            //   use: extractLESS.extract([ 'css-loader', 'less-loader' ])
+            // }
         ]
     },
     plugins:[
-        extractCSS ,
-        extractLESS,
+        // extractCSS ,
+        // extractLESS,
+        new webpack.HotModuleReplacementPlugin(),
         new HtmlWebpackPlugin({
-            title: 'lucia',
-            template: "./index.html",
-
+            template: path.join(__dirname, "src/index.html"),
+            filename: 'index.html',
+            chunks: ['index'],
+            inject: true,
+            minify: {
+                html5: true,
+                collapseWhitespace: true,
+                preserveLineBreaks: false,
+                minifyCSS: true,
+                minifyJS: true,
+                removeComments: false,
+            }
         }),
         new ProgressBarPlugin({
             format: 'build [:bar] ' + chalk.green.bold(':percent') + ' (:elapsed seconds)',
@@ -70,15 +75,15 @@ module.exports = {
     ],
     devServer: {
         contentBase: path.join(__dirname, "dist"),
-        compress: true,//压缩
+        // compress: true,//压缩
         port: 3000,//端口
         open: true,//编译完之后在默认浏览器打开
         // clientLogLevel: "none",
         hot: true,
     },
-    stats: "normal",
-    watchOptions: {
-        aggregateTimeout: 300,
-        poll: 1000
-    }
+    // stats: "normal",
+    // watchOptions: {
+    //     aggregateTimeout: 300,
+    //     poll: 1000
+    // }
 };
